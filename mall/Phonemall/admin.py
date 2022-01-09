da959caca936c120a3259fafd87fccd576c0a0b9
lzb <2324369886@qq.com>
2022-01-09 06:05:53 -0800

    admin站点

--- mall/Phonemall/admin.py
+++ mall/Phonemall/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+import Phonemall.models
 # Register your models here.
+
+admin.site.register(Phonemall.models.BookInfo)
+admin.site.register(Phonemall.models.PeopleInfo)
