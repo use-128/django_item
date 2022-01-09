8ddb00ddab11c35cf2287a00c94d4695de7ad8e9
lzb <2324369886@qq.com>
2022-01-09 06:11:15 -0800

    定义视图函数

--- mall/Phonemall/views.py
+++ mall/Phonemall/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from django.http import HttpResponse
 # Create your views here.
+"""
+视图就是python函数
+
+"""
+
+
+def index(request):
+
+    return HttpResponse("ok")
